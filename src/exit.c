e25d03cfb7d62f7f970599e65914104a25b758ce
Robin Linden <dev@robinlinden.eu>
2020-03-13 00:42:15 +0100

    Implement atexit

--- src/exit.c
+++ src/exit.c
@@ -1,6 +1,15 @@
 #include "stdlib.h"
 
+#define HANDLERS 32
+static void (*_exit_handlers[HANDLERS])(void);
+
 void exit(int exit_code) {
+    for (int i = 0; i < HANDLERS; ++i) {
+        if (_exit_handlers[i]) {
+            _exit_handlers[i]();
+        }
+    }
+
 #if _WIN64 || __CYGWIN__
     volatile register int rdi asm("%rcx") = exit_code;
     asm("call ExitProcess");
@@ -10,4 +19,15 @@ void exit(int exit_code) {
     asm("syscall");
 #endif
     while (1) {}
+}
+
+int atexit(void (*func)(void)) {
+    static int i = HANDLERS - 1;
+
+    if (i < 0) {
+        return 1;
+    }
+
+    _exit_handlers[i--] = func;
+    return 0;
 }
--- test/src/main.c
+++ test/src/main.c
@@ -1,10 +1,23 @@
 #include "stdbool.h"
 #include "stdlib.h"
 
+void do_thing() {
+    volatile int a = 1;
+    (void)a;
+}
+
+void do_more() {
+    volatile int *b = 0;
+    (void)b;
+}
+
 int main2() {
     bool a = false;
     bool b = !a;
     (void)b;
+
+    atexit(do_more);
+    atexit(do_thing);
 
     exit(100);
     return 50;
